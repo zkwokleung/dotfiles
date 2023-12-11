dd0d963b66bb08647f9c62171a789bbfdbd8edf2
Andrew SZETO <zkwokleung@gmail.com>
2023-12-12 05:12:06 +0800

    update: minor changes on vim conf

--- vim/lua/lang/lsp.lua
+++ vim/lua/lang/lsp.lua
@@ -8,7 +8,7 @@ require("mason-lspconfig").setup({
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 for _, s in ipairs(lsps) do
-    require('lspconfig')[s].setup {
+    lsp[s].setup {
         capabilities = capabilities
     }
 end
