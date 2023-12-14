d5ab18b98f0668701ae30e0927c7a8dce1a09e52
Andrew SZETO <zkwokleung@gmail.com>
2023-12-14 23:23:27 +0800

    Revert "update: dashboard"
    This reverts commit 54c0ad3b11aa03437866ebba22fe14d36c8eec59.

--- vim/lua/core/lazy.lua
+++ vim/lua/core/lazy.lua
@@ -1,131 +1,111 @@
 require("lazy").setup(
-{
-  -- Package Manager
-  {
-    "williamboman/mason.nvim",
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
+    {
+        -- Package Manager
+        {
+            "williamboman/mason.nvim",
+        },
+        {
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
+        },
 
-  -- LSP
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-    },
-    lazy = false,
-  },
+        -- LSP
+        {
+            "williamboman/mason-lspconfig.nvim",
+            dependencies = {
+                "neovim/nvim-lspconfig",
+            },
+            lazy = false,
+        },
 
-  -- DAP
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      "theHamsta/nvim-dap-virtual-text",
-      "rcarriga/nvim-dap-ui",
-      "mfussenegger/nvim-dap-python",
-      "Pocco81/DAPInstall.nvim",
-    },
-  },
+        -- DAP
+        {
+            "mfussenegger/nvim-dap",
+            dependencies = {
+                "theHamsta/nvim-dap-virtual-text",
+                "rcarriga/nvim-dap-ui",
+                "mfussenegger/nvim-dap-python",
+                "Pocco81/DAPInstall.nvim",
+            },
+        },
 
-  -- Lint
-  {
-    "mfussenegger/nvim-lint",
-  },
+        -- Lint
+        {
+            "mfussenegger/nvim-lint",
+        },
 
-  -- Formatter
-  {
-    "mhartington/formatter.nvim",
-  },
+        -- Formatter
+        {
+            "mhartington/formatter.nvim",
+        },
 
-  -- UI
-  {
-    'nvimdev/dashboard-nvim',
-    event = 'VimEnter',
-    config = function()
-      require('dashboard').setup {
-        -- config
-      }
-    end,
-    dependencies = { {'nvim-tree/nvim-web-devicons'}}
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        -- UI
+        {
+            'goolord/alpha-nvim',
+        },
+        {
+            "nvim-treesitter/nvim-treesitter",
+            build = ":TSUpdate",
+        },
+        {
+            "nvim-neo-tree/neo-tree.nvim",
+            branch = "v3.x",
+            dependencies = {
+                "nvim-lua/plenary.nvim",
+                "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+                "MunifTanjim/nui.nvim",
+                -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+            }
+        },
+        {
+            "rcarriga/nvim-notify",
+        },
+        {
+            "nvim-lualine/lualine.nvim",
+        },
+
+        -- Completion
+        {
+            "folke/noice.nvim",
+            event = "VeryLazy",
+            dependencies = {
+                -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+                "MunifTanjim/nui.nvim",
+                "rcarriga/nvim-notify",
+            },
+        },
+        {
+            "jiangmiao/auto-pairs",
+        },
+
+        -- Snippet
+        {
+            "L3MON4D3/LuaSnip",
+            version = "v2.*",
+        },
+        {
+            "hrsh7th/nvim-cmp",
+            dependencies = {
+                "hrsh7th/cmp-buffer",
+                "hrsh7th/cmp-nvim-lsp",
+                "hrsh7th/cmp-path",
+                "hrsh7th/cmp-nvim-lua",
+                "hrsh7th/cmp-vsnip",
+                "hrsh7th/cmp-cmdline",
+                "neovim/nvim-lspconfig",
+                "L3MON4D3/LuaSnip",
+                "saadparwaiz1/cmp_luasnip"
+            },
+        },
+
+        -- Theme
+        {
+            "folke/tokyonight.nvim",
+            lazy = false,
+            priority = 1000,
+        },
+
+        {
+            "github/copilot.vim"
+        }
     }
-  },
-  {
-    "rcarriga/nvim-notify",
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-  },
-
-  -- Completion
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    },
-  },
-  {
-    "jiangmiao/auto-pairs",
-  },
-
-  -- Snippet
-  {
-    "L3MON4D3/LuaSnip",
-    version = "v2.*",
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-nvim-lua",
-      "hrsh7th/cmp-vsnip",
-      "hrsh7th/cmp-cmdline",
-      "neovim/nvim-lspconfig",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip"
-    },
-  },
-
-  -- File search
-  {
-    'wincent/command-t',
-    run = 'cd lua/wincent/commandt/lib && make',
-    setup = function()
-      vim.g.CommandTPreferredImplementation = 'lua'
-    end,
-    config = function()
-      require('wincent.commandt').setup({
-      })
-    end,
-  },
-
-  -- Theme
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-
-  {
-    "github/copilot.vim"
-  }
-}
 )
