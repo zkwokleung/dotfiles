1be043ce2ac85cbf103b8cc94ef623268976468b
Andrew SZETO <zkwokleung@gmail.com>
2023-12-16 07:46:34 +0800

    update: vim format options autocmd

--- vim/lua/core/autocmd.lua
+++ vim/lua/core/autocmd.lua
@@ -3,12 +3,23 @@ local autocmd = vim.api.nvim_create_autocmd
 local cmd = vim.api.nvim_create_user_command
 local namespace = vim.api.nvim_create_namespace
 
+-- File type
+autocmd({ "FileType" }, {
+	desc = "Set format options for all file types",
+	group = augroup("filetype", { clear = true }),
+	pattern = "*",
+	callback = function()
+		vim.cmd("set formatoptions-=cro")
+		vim.cmd("set formatoptions+=tqjnl")
+	end,
+})
+
 -- Format on save
-autocmd({ 'BufWritePre' }, {
-    desc = 'Format on save',
-    group = augroup("files", { clear = true }),
-    callback = function()
-        -- Call AutoFormat
-        vim.cmd('Autoformat')
-    end,
+autocmd({ "BufWritePre" }, {
+	desc = "Format on save",
+	group = augroup("files", { clear = true }),
+	callback = function()
+		-- Call AutoFormat
+		vim.cmd("Autoformat")
+	end,
 })
