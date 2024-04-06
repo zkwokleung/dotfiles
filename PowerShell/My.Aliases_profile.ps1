5965ca942238ce1a93d881bbb507e8b1bb861dbd
Andrew SZE-TO <zkwokleung@gmail.com>
2024-04-07 03:38:04 +0800

    update: power shell aliases

--- PowerShell/My.Aliases_profile.ps1
+++ PowerShell/My.Aliases_profile.ps1
@@ -3,3 +3,11 @@ Set-Alias g git
 
 # * Python & Conda
 Set-Alias py python
+
+# npm
+Set-Alias n pnpm
+Set-Alias npm pnpm
+Set-Alias npx "pnpm exec"
+Set-Alias ni "pnpm install"
+Set-Alias ns "pnpm start"
+
