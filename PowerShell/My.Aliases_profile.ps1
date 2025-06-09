3df4a0a3a9c308bc853f75f3a1ebc7850c41a06d
Andrew SZETO <zkwokleung@gmail.com>
2025-06-09 14:52:24 +0800

    Moved powershell config files to another repository

--- PowerShell/My.Aliases_profile.ps1
+++
@@ -1,5 +0,0 @@
-# * Git
-Set-Alias g git
-
-# * Python & Conda
-Set-Alias py python
--- PowerShell/bootstrap.ps1
+++
@@ -1,7 +0,0 @@
-# Copy the PowerShell setting files to the correct locations
-
-# Copy the profiles
-Copy-Item -Path "$PSScriptRoot\*.ps1" -Destination "$env:USERPROFILE\Documents\PowerShell\" -Force
-
-# Copy the themes
-Copy-Item -Path "$PSScriptRoot\themes\*.json" -Destination "$env:POSH_THEMES_PATH\" -Force
